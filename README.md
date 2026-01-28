5274fbba12a6d0bf488c4cccde230d4a3106e095
Nathan Wanjongkhum <wanjongkhumnathan@gmail.com>
2026-01-28 14:13:39 -0800

    Moved to empty template

--- README.md
+++
@@ -1,7 +0,0 @@
-# SuperMarioBros
-
-1. Please read the [**contributing guideline**](https://github.com/algorithm0r/SuperMarioBros/blob/hungvu/contributing-guideline/CONTRIBUTING.md).
-
-## Contributors
-
-- Chris Marriott
--- test/test.js
+++
@@ -1,7 +0,0 @@
-let assert = require('assert');
-// This test case exists to fullfill the requirement of Github Action.
-describe('Place holder', function() {
-  it('This returns true', function() {
-    assert.strictEqual(1, 1)
-  });
-});
