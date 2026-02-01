cf2a27bcd1f4fb8ff0947615edb0ebcbf20024f2
xcforcristianx <159111515+xcforcristianx@users.noreply.github.com>
2026-01-31 22:33:21 -0800

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,19 +1,17 @@
-// This game shell was happily modified from Googler Seth Ladd's "Bad Aliens" game and his Google IO talk in 2011
-
 class Timer {
-    constructor() {
-        this.gameTime = 0;
-        this.maxStep = 0.05;
-        this.lastTimestamp = 0;
-    };
+  constructor() {
+    this.gameTime = 0;
+    this.maxStep = 0.05;
+    this.wallLastTimestamp = 0;
+  }
 
-    tick() {
-        const current = Date.now();
-        const delta = (current - this.lastTimestamp) / 1000;
-        this.lastTimestamp = current;
+  tick() {
+    const wallCurrent = performance.now();
+    const wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
+    this.wallLastTimestamp = wallCurrent;
 
-        const gameDelta = Math.min(delta, this.maxStep);
-        this.gameTime += gameDelta;
-        return gameDelta;
-    };
-};
+    const gameDelta = Math.min(wallDelta, this.maxStep);
+    this.gameTime += gameDelta;
+    return gameDelta;
+  }
+}
