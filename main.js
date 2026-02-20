c66e958a2b06d8dab8909f02e14e5a5030e555fd
CYoung5233 <cyoung17@uw.edu>
2026-02-20 12:53:22 -0800

    Moved level specifics to their own file out of main

--- main.js
+++ main.js
@@ -67,54 +67,18 @@ ASSET_MANAGER.downloadAll(() => {
 
   gameEngine.mode = "menu";
 
-  gameEngine.currentLevel = 1;
+  gameEngine.currentLevel = 1; //switch to 0 as initial state marking not yet in level?
 
 
   // Builds a fresh set of game entities (used for initial load and replay)
   function buildWorld(engine) {
     engine.addEntity(new Background(engine));
 
+    //Place specific levels in their own file for better organization
     // Level-specific spawns
-    if (engine.currentLevel === 1) {
-      engine.addEntity(new Ghost(engine, 700, 50));
-      engine.addEntity(new Ghost(engine, 775, 350));
-      engine.addEntity(new Ghost(engine, 300, 400));
-      engine.addEntity(new Sheep(engine, 500, 50));
-
-    } else if (engine.currentLevel === 2) {
-      const spiderPath = [
-        { x: 400, y: 0 },
-        { x: 600, y: 0 },
-      ];
-      // Add blocks 
-      const builder = new LevelBuilder(engine);
-
-      builder.spawnRow(15, 0, 20); 
-      builder.spawnBlock(5, 14);
-      builder.spawnBlock(5, 13);
-
-      engine.addEntity(new Demon(engine, 300, 400))
-      engine.addEntity(new Sheep(engine, 100, 200));
-      engine.addEntity(new Spider(engine, spiderPath));
-      engine.addEntity(new Ghost(engine, 700, 50));
-
-    } else {
-      // default/fallback
-      engine.addEntity(new Ghost(engine, 700, 50));
-      // Collection of some items - for demonstration purposes
-      gameEngine.addEntity(new PickupItem(gameEngine, 220, 140, "Sword"));
-      gameEngine.addEntity(new PickupItem(gameEngine, 280, 140, "ToothBrush"));
-      gameEngine.addEntity(new PickupItem(gameEngine, 340, 140, "TeddyBear"));
-
-      gameEngine.addEntity(new PickupItem(gameEngine, 220, 220, "DreamCatcher"));
-      gameEngine.addEntity(new PickupItem(gameEngine, 280, 220, "Rocket"));
-      gameEngine.addEntity(new PickupItem(gameEngine, 340, 220, "Pajama"));
-    }
+    Levels.buildLevel(engine)
 
     // Common entities
-    engine.addEntity(new Bed(engine, 700, 300));
-    engine.addEntity(new SleepyGuy(engine, 100, 100));
-    engine.addEntity(new WaypointBuilder(engine));
     engine.addEntity(new EndGame(engine));
     engine.addEntity(new MenuRoomController(engine));
 
